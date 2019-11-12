caef836fc03e20e61bdcc432d31fdcebe9150897
tatoren <one_with_all@hotmail.com>
2019-11-12 10:48:06 -0700

    AddBasket

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/201911121743557_AddBasket.cs
@@ -0,0 +1,43 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBasket : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BasketItems",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        BasketID = c.String(maxLength: 128),
+                        ProductID = c.String(),
+                        Quantity = c.Int(nullable: false),
+                        CreatedDate = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Baskets", t => t.BasketID)
+                .Index(t => t.BasketID);
+            
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        CreatedDate = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BasketItems", "BasketID", "dbo.Baskets");
+            DropIndex("dbo.BasketItems", new[] { "BasketID" });
+            DropTable("dbo.Baskets");
+            DropTable("dbo.BasketItems");
+        }
+    }
+}
