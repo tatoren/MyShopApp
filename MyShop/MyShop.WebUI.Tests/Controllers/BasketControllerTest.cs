4aa295ff136cbf86f2186df70230fce9e9c96215
tatoren <one_with_all@hotmail.com>
2019-11-12 14:57:13 -0700

    Add UnitTests

---
+++ MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using MyShop.Services;
+using MyShop.WebUI.Controllers;
+using MyShop.WebUI.Tests.Mocks;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTest
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            //Setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+            var controller = new BasketController(basketService);
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+
+            //Act
+            //basketService.AddToBasket(httpContext, "1");
+            controller.AddToBasket("1");
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            //Assert
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductID);
+        }
+        [TestMethod]
+        public void CanGetSummaryViewModel()
+        {
+            //Setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+            products.Insert(new Product
+            {
+                ID = "1",
+                Price = 10.00m
+            });
+            products.Insert(new Product
+            {
+                ID = "2",
+                Price = 5.00m
+            });
+            products.Insert(new Product
+            {
+                ID = "3",
+                Price = 7.00m
+            });
+
+            //Act
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem { ProductID = "1", Quantity = 2 });
+            basket.BasketItems.Add(new BasketItem { ProductID = "2", Quantity = 1 });
+            basket.BasketItems.Add(new BasketItem { ProductID = "3", Quantity = 1 });
+            baskets.Insert(basket);
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+            var httpContext = new MockHttpContext();
+            httpContext.Request.Cookies.Add(new System.Web.HttpCookie("eCommerceBasket") { Value = basket.ID });
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            var result = controller.BasketSummary() as PartialViewResult;
+            var basketSummary = (BasketSummaryViewModel)result.ViewData.Model;
+            
+            //Assert
+            Assert.AreEqual(4, basketSummary.BasketCount);
+            Assert.AreEqual(32.00m, basketSummary.BasketTotal);
+
+
+        }
+    }
+}
---
+++ MyShop/MyShop.WebUI.Tests/Mocks/MockContext.cs
@@ -0,0 +1,76 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.WebUI.Tests.Mocks
+{
+    public class MockContext<T> : IRepository<T> where T :BaseEntity
+    {
+        List<T> items;
+        string className;
+
+        public MockContext()
+        {
+           items = new List<T>();
+        }
+
+        public void Commit()
+        {
+            return;
+        }
+
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            T tToUpdate = items.Find(i => i.ID == t.ID);
+            if (tToUpdate != null)
+            {
+                tToUpdate = t;
+            }
+            else
+            {
+                throw new Exception(className + " can not be found.");
+            }
+        }
+
+        public T Find(string ID)
+        {
+            T t = items.Find(i => i.ID == ID);
+            if (t != null)
+            {
+                return t;
+            }
+            else
+            {
+                throw new Exception(className + " can not be found");
+            }
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(string ID)
+        {
+            T tToDelete = items.Find(i => i.ID == ID);
+
+            if (tToDelete != null)
+            {
+                items.Remove(tToDelete);
+            }
+            else
+            {
+                throw new Exception(className + "can not be found.");
+            }
+        }
+    }
+}
